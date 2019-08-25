51cb583b72bf7df17b4a60c5051b4749a23332a5
Timothy Martinez <tmrcmartinez@gmail.com>
2019-08-24 20:28:02 -0400

    added a lot of styling

--- www/my_projects.js
+++ www/my_projects.js
@@ -1,34 +1,43 @@
 'use strict';
+
 const e = React.createElement
 
 class MyProjects extends React.Component {
   state = {
-    projectList: [
-      {pageOne: "page1", pageTwo: "page2", pageThree: "page3", pageFour: "page4" }
-    ]
+    projectList: []
   }
 
   render() {
-    return e('div', {},
-      e('h1', {id: 'show'}, 'hohohohohoho'),
-      e('h1', {id: 'nosho'}, 'hahahahahah'),
-      e('button', {type: 'submit', className: 'selector', id: 'site1'}, '')
+    return e('div', {id: 'config-container'},
+      e('div', {id: 'site-one', className: 'nosho'}, ''),
+      e('div', {id: 'site-two', className: 'show'}, ''),
+      e('div', {id: 'site-three', className: 'nosho'}, ''),
+      e('div', {id: "button-container"}, 
+        e('button', {type: 'submit', className: 'selector site1'}, ''),
+        e('button', {type: 'submit', className: 'selector site2'}, ''),
+        e('button', {type: 'submit', className: 'selector site3'}, ''),
+      )
     )
   }
 }
 
-const runClick = (event) => {
-  page
-  console.log(event.target)
-}
-
-function shelf() {
-  if( document.getElementById("site1").classList.contains('site1')){
-    document.getElementById("site1").classList.add('shelf-op1')
-    document.getElementById("shelf-select").classList.remove('shelf-op0')
-  } else {
-    document.getElementById("shelf-select").classList.add('shelf-op0')
-    document.getElementById("shelf-select").classList.remove('shelf-op1')
+function runClick(event) {
+  console.log(event.target.classList)
+  if(event.target.classList.contains('site1')) {
+    document.getElementsByClassName("show")[0].classList.add('nosho')
+    document.getElementsByClassName("show")[0].classList.remove('show')
+    document.getElementById("site-one").classList.add('show')
+    document.getElementById("site-one").classList.remove('nosho')
+  } else if (event.target.classList.contains('site2')) {
+    document.getElementsByClassName("show")[0].classList.add('nosho')
+    document.getElementsByClassName("show")[0].classList.remove('show')
+    document.getElementById("site-two").classList.add('show')
+    document.getElementById("site-two").classList.remove('nosho')
+  } else if(event.target.classList.contains('site3')) {
+    document.getElementsByClassName("show")[0].classList.add('nosho')
+    document.getElementsByClassName("show")[0].classList.remove('show')
+    document.getElementById("site-three").classList.add('show')
+    document.getElementById("site-three").classList.remove('nosho')
   }
 }
 
@@ -36,8 +45,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const myPages = document.querySelector('#my_pages')
   ReactDOM.render(e(MyProjects), myPages)
   
-  const select = document.querySelector('.selector')
-  select.addEventListener('click', runClick)
+  document.addEventListener('click', runClick)
 })
 
 
